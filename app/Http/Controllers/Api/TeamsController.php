2b1717fdf5eddc2efcc011ad8694493b5a323795
Ted-Rose <tedis.rozenfelds@gmail.com>
2023-09-30 16:07:02 -0700

    Changing player and team points enabled in be

--- app/Http/Controllers/Api/TeamsController.php
+++ app/Http/Controllers/Api/TeamsController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller;
 use App\Models\Team;
 use App\Models\Player;
 use Illuminate\Http\Request;
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
-use App\Http\Controllers\TeamsController;
-use App\Http\Controllers\Api\AuthController;
-use App\Http\Controllers\PlayersController;
+// use App\Http\Controllers\Api\TeamsController;
+// use App\Http\Controllers\Api\AuthController;
+// use App\Http\Controllers\PlayersController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,30 +17,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::prefix('teams')->group(function () {
-    Route::get('/', [TeamsController::class, 'index'])->name('team.index');
-    Route::get('/{id}', [TeamsController::class, 'show'])->name('team.show');
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//   return $request->user();
+// });
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-  return $request->user();
-});
+// Route::prefix('teams')->group(function () {
+//     Route::get('/', [TeamsController::class, 'index'])->name('team.index');
+//     Route::get('/{id}', [TeamsController::class, 'show'])->name('team.show');
+// });
 
-Route::prefix('change-team-points')->group(function () {
-    Route::post('/', [TeamsController::class, 'changePoints']);
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//   return $request->user();
+// });
 
-Route::prefix('players')->group(function () {
-    Route::get('/', [PlayersController::class, 'index'])->name('player.index');
-    Route::get('/{id}', [PlayersController::class, 'show'])->name('player.show');
-});
+// Route::prefix('change-team-points')->group(function () {
+//     Route::post('/', [TeamsController::class, 'changePoints']);
+// });
 
-Route::prefix('change-player-points')->group(function () {
-    Route::post('/', [PlayersController::class, 'changePoints']);
-});
+// Route::prefix('players')->group(function () {
+//     Route::get('/', [PlayersController::class, 'index'])->name('player.index');
+//     Route::get('/{id}', [PlayersController::class, 'show'])->name('player.show');
+// });
 
-Route::prefix('change-player-points')->group(function () {
-    Route::post('/', [PlayersController::class, 'changePoints']);
-});
+// Route::prefix('change-player-points')->group(function () {
+//     Route::post('/', [PlayersController::class, 'changePoints']);
+// });
 
-Route::post("/login", [AuthController::class, "login"]);
+// Route::prefix('change-player-points')->group(function () {
+//     Route::post('/', [PlayersController::class, 'changePoints']);
+// });
+
+// Route::post("/login", [AuthController::class, "login"]);
